5ee70a5e79a5ab41a88179903e781efd5b842a7f
Pramodengnbj <pramodgangwar753@gmail.com>
2026-01-28 22:20:51 +0530

    Update

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -3,3 +3,15 @@ import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/cartSlice";
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios.get(`https://dummyjson.com/products/${id}`)
+      .then(res => { setProduct(res.data); setLoading(false); })
+      .catch(() => { setError("Product not found"); setLoading(false); });
+  }, [id]);
