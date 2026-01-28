28c47d28b05b48e1b3075a14df67fae75ba5c1d4
Pramodengnbj <pramodgangwar753@gmail.com>
2026-01-28 22:14:54 +0530

    update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,3 +2,8 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CartItem from "./CartItem";
 import { Link } from "react-router-dom";
+const Cart = () => {
+  const cartItems = useSelector(state => state.cart.cartItems);
+  const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+  if(cartItems.length === 0) return <p style={{textAlign:'center'}}>Cart is empty. <Link to="/">Shop now</Link></p>;
