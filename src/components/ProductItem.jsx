c5b99279141021cba8f557fc793756557407b481
Pramodengnbj <pramodgangwar753@gmail.com>
2026-01-28 22:21:58 +0530

    ProductItem

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
+
+const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
