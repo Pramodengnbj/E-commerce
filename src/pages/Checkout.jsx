d8ec454b30079ac03b61133ae80b3b6abdb1afde
Pramodengnbj <pramodgangwar753@gmail.com>
2026-01-28 22:25:34 +0530

    Change

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,3 +2,10 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { clearCart } from "../redux/cartSlice";
 import { useNavigate } from "react-router-dom";
+const Checkout = () => {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const cartItems = useSelector(state => state.cart.cartItems);
+  const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
